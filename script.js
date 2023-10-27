ed282087d4c95de4a52cd5e324c8b71f81733dc0
Irfan khan <80601941+ik-deep@users.noreply.github.com>
2023-10-27 20:01:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+function delayMessage() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("Hello, world!");
+        }, 1000); // Resolves after 1 second
+    });
+}
+
+// Get the HTML element with the ID "output"
+const outputElement = document.getElementById("output");
+
+// Call the function and handle the resolved promise
+delayMessage()
+    .then((message) => {
+        // Update the text of the "output" element
+        outputElement.textContent = message;
+    })
+    .catch((error) => {
+        // Handle any errors here
+        console.error(error);
+    });
